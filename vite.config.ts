121cf1272a068eb911cb98efb04b990179e30cb6
tenyon <meettyj.@163.com>
2024-12-07 20:19:28 +0800

    fix: requestType已废除

--- vite.config.ts
+++ vite.config.ts
@@ -56,7 +56,7 @@ export default defineConfig(({ command, mode }) => {
     ],
     resolve: {
       alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        '@': srcPath,
       },
     },
     css: {
